6a10f731154acbc06d9ce1e996672ceee8ded50c
nrv2391 <navemen23@hotmail.com>
2025-07-22 22:51:47 -0600

    segundo ejemplo de chat de supermercado

---
+++ 02-chatbot-negocio-open-ai/app.js
@@ -0,0 +1,91 @@
+import express from "express";
+import axios from "axios";
+import { OpenAI } from "openai/client.js";
+import { config } from "dotenv";
+import http from "http";
+import cors from "cors";
+// cargar datos de entorno
+
+config();
+// instacia de express
+
+// configurar open ai
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY
+});
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+const PORT = process.env.PORT || 3000;
+
+// agregar rutas de la API
+
+// servir archivos estáticos
+
+
+app.post("/api/chat", async (req, res) => {
+  try {
+    const promptRole = "system";
+    const promptContext = `
+      Eres una asistente de soporte para un supermercado.
+      Tu tarea es ayudar a los clientes con sus preguntas y problemas relacionados con el supermercado.
+
+      Información del supermercado:
+      - Horario de atención: Lunes a Sábado de 8:00 a 20:00, Domingo de 9:00 a 18:00.
+      - Ubicación: Calle Falsa 123, Ciudad.
+      - Teléfono: 123-456-7890. 
+      - Servicios: Venta de alimentos, productos de limpieza, atención al cliente.
+      - Promociones actuales: 10% de descuento en frutas y verduras, 20% de descuento en productos de limpieza.
+      - Políticas: No se aceptan devoluciones de productos perecederos, se aceptan devoluciones de productos no perecederos dentro de los 30 días.
+      - Productos en venta : Leche, Pan, Frutas y verduras, Productos de limpieza, Bebidas, Carnes y pescados, Productos enlatados, Cereales y pastas, Snacks y golosinas, Productos de higiene personal.
+      - Marcas de los productos: Leche: Marca A, Marca B; Pan: Marca C, Marca D; Frutas y verduras: Marca E, Marca F; Productos de limpieza: Marca G, Marca H; Bebidas: Marca I, Marca J; Carnes y pescados: Marca K, Marca L; Productos enlatados: Marca M, Marca N; Cereales y pastas: Marca O, Marca P; Snacks y golosinas: Marca Q, Marca R; Productos de higiene personal: Marca S, Marca T. 
+  
+      - Información adicional: El supermercado ofrece servicio a domicilio, acepta tarjetas de crédito y débito, y tiene una sección de productos orgánicos.
+      - Preguntas frecuentes: ¿Cuál es el horario de atención?, ¿Dónde está ubicado el`;
+
+    const promptConstraint = `
+      Responde de manera clara y concisa. Solamente la información que se te ha proporcionado.
+      Si no tienes suficiente información, indica que no puedes ayudar con esa solicitud.
+      No uses emojis ni lenguaje informal.
+      No uses lenguaje técnico o jerga que un cliente promedio no entendería.
+      No hagas suposiciones sobre la información que no se te ha proporcionado.
+      No respondas preguntas que no estén relacionadas con el supermercado.
+      No uses lenguaje que pueda ser considerado ofensivo o inapropiado.
+      Cualquier otra pregunta que no esté relacionada con el supermercado, indica que no puedes ayudar con esa solicitud.
+      Usa los mínimos tokens posibles para responder.
+    `;
+
+    const { message } = req.body;
+
+    if (!message)
+      return res.status(400).json({ error: "El mensaje es requerido" });
+
+    const response = await openai.chat.completions.create({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { role: promptRole, content: promptContext },
+        { role: "user", content: message },
+        { role: promptRole, content: promptConstraint }
+      ],
+      max_tokens: 200
+    });
+
+    const { content } = response.choices[0].message;
+
+    return res.json({
+      message: content
+    });
+  } catch (error) {
+    console.error("Error al procesar la solicitud:", error);
+    res.status(500).json({ error: "Error al procesar la solicitud" });
+  }
+});
+
+app.use("/chat", express.static("public"));
+
+http.createServer(app).listen(PORT, () => {
+  console.log(`Servidor corriendo en http://localhost:${PORT}`);
+});
---
+++ 02-chatbot-negocio-open-ai/public/assets/js/main.js
@@ -0,0 +1,50 @@
+const sendButton = document.getElementById("sendButton");
+
+document.addEventListener("keydown", async e => {
+  if (e.key === "Enter") {
+    try {
+      e.preventDefault();
+      const inputText = document.getElementById("inputText");
+
+      if (!inputText.value.trim()) {
+        alert("Por favor, escribe un mensaje antes de enviar.");
+        return;
+      }
+
+      const repsonse = await fetch("http://localhost:3000/api/chat", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify({ message: inputText.value })
+      });
+
+      if (!repsonse.ok) {
+        alert("Error al enviar el mensaje. Por favor, inténtalo de nuevo.");
+        return;
+      }
+
+      const data = await repsonse.json();
+      console.log("Respuesta del bot:", data.message);
+      const chatContainer = document.querySelector(".chat__messages");
+      const messageUserElement = document.createElement("div");
+      const messageBotElement = document.createElement("div");
+
+      messageUserElement.classList = "chat__message chat__message--user";
+      messageBotElement.classList = "chat__message chat__message--bot";
+
+      messageBotElement.textContent = data.message;
+      messageUserElement.textContent = inputText.value;
+
+      chatContainer.append(messageUserElement);
+      chatContainer.append(messageBotElement);
+      inputText.value = ""; // Limpiar el campo de entrada
+      chatContainer.scrollTop = chatContainer.scrollHeight; // Desplazar hacia abajo para mostrar
+    } catch (error) {
+      console.error("Error al enviar el mensaje:", { error });
+      alert(
+        "Ocurrió un error al enviar el mensaje. Por favor, inténtalo de nuevo más tarde."
+      );
+    }
+  }
+});
